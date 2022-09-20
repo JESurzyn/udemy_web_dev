29dd1683823801fc86602961394a585a558fefb2
JESurzyn <jordan.surzyn@gmail.com>
2022-09-19 20:59:53 -0700

    javascript for loops

---
+++ JavaScript_tutorials/for_loops/app.js
@@ -0,0 +1,36 @@
+// for (let i =1; i<=10; i++){
+//     console.log(i);
+// }
+
+// for (let i = 0; i <= 20; i += 2) {
+//     console.log(i);
+// }
+
+// for (let i = 1; i <= 20; i += 2) {
+//     console.log(i);
+// }
+
+// for (let i = 100; i >= 0; i -= 10) {
+//     console.log(i);
+// }
+
+// for (let i = 10; i <= 1000; i *= 10) {
+//     console.log(i);
+// }
+
+let randoArray = [
+    'rand1',
+    'rand2',
+    'rand3',
+    'rand4',
+    'rand5',
+    'rand6',
+]
+
+for (let i = 0; i < randoArray.length; i++) {
+    console.log(randoArray[i]);
+}
+
+for (let i = randoArray.length -1; i >= 0; i--) {
+    console.log(randoArray[i]);
+}
