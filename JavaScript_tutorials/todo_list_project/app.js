d0ffa03aea3f8c14ad81ba5ecba3c92cb91737ed
JESurzyn <jordan.surzyn@gmail.com>
2022-09-23 19:21:35 -0700

    to-list mini proj

---
+++ JavaScript_tutorials/todo_list_project/app.js
@@ -0,0 +1,21 @@
+let input = ''
+let tdList = []
+while(input !== 'quit') {
+    input = prompt('What would you like to do?');
+    if (input ==='new') {
+        let item = prompt('Add item')
+        tdList.push(item)
+        console.log(`${item} added to the list`)
+    } else if (input == 'list') {
+        let decor = '**********'
+        console.log(decor)
+        for (let i = 0; i < tdList.length; i++) {
+            console.log(`${i}: ${tdList[i]}`)
+        }
+        console.log(decor)
+    } else if (input === 'delete') {
+        let toDelete = parseInt(prompt("What's the index of the item you want to delete?"))
+        deleted = tdList.splice(toDelete,1)
+        console.log(`${deleted[0]} successfully deleted`)
+    }
+}
