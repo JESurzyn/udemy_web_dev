b6ee7a625a55c5601bafeb4772b16e950571fc5c
JESurzyn <jordan.surzyn@gmail.com>
2022-09-23 19:37:38 -0700

    more mods to-do proj

--- JavaScript_tutorials/todo_list_project/app.js
+++ JavaScript_tutorials/todo_list_project/app.js
@@ -3,19 +3,19 @@ const tdList = []
 while(input !== 'quit') {
     input = prompt('What would you like to do?');
     if (input ==='new') {
-        let item = prompt('Add item')
+        let item = prompt('Add item');
         tdList.push(item)
-        console.log(`${item} added to the list`)
+        console.log(`${item} added to the list`);
     } else if (input == 'list') {
         let decor = '**********'
-        console.log(decor)
+        console.log(decor);
         for (let i = 0; i < tdList.length; i++) {
             console.log(`${i}: ${tdList[i]}`)
         }
-        console.log(decor)
+        console.log(decor);
     } else if (input === 'delete') {
-        let toDelete = parseInt(prompt("What's the index of the item you want to delete?"))
-        deleted = tdList.splice(toDelete,1)
-        console.log(`${deleted[0]} successfully deleted`)
+        let toDelete = parseInt(prompt("What's the index of the item you want to delete?"));
+        deleted = tdList.splice(toDelete,1);
+        console.log(`${deleted[0]} successfully deleted`);
     }
 }
