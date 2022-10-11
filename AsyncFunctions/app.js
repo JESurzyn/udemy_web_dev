ab24bfa10c194101bc690b44797e8a0b8816f65f
JESurzyn <jordan.surzyn@gmail.com>
2022-10-10 18:54:52 -0700

    async and promises

---
+++ AsyncFunctions/app.js
@@ -0,0 +1,20 @@
+//declaring a function as async automatically returns a promise
+// async function hello() {
+
+// }
+
+//arrow function syntax for declaring async functions
+// const sing = async () => {
+//     // throw new Error("UH OH");
+//     throw "OH NO PROBLEM";
+//     return 'LALALALA';
+// }
+
+// sing()
+//     .then(data => {
+//     console.log('Promise resolved with:', data)
+//     })
+//     .catch(err => {
+//         console.log('OH NO, PROMISE REJECTED')
+//         console.log(err)
+//     })
