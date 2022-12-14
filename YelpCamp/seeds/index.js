75fa7606ead277b42bbe85274019961b1c8c4af6
JESurzyn <jordan.surzyn@gmail.com>
2022-12-14 08:34:05 -0800

    styling index page

--- YelpCamp/seeds/index.js
+++ YelpCamp/seeds/index.js
@@ -27,9 +27,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel, dolore? Eligendi omnis veniam recusandae ab cumque labore dignissimos quas praesentium nulla sequi, blanditiis assumenda eveniet minima ad ea mollitia ratione.',
+            price
         })
         await camp.save();
     }
